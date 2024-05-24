8e4d451e73a900ec7ae0a3b7b71f674d615cf7c7
kevinR <kevinroesner94@gmail.com>
2024-05-24 12:21:53 +0200

    Basic draw function and creating the grid are working

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,1 +1,35 @@
 const sketchContainer = document.querySelector('.draw-flex-container');
+let inputElement = document.querySelector('.input-value');
+let value = inputElement.value;
+let numDiv = value*value;
+function createGrid(value){
+  numDiv = value*value;
+  sketchContainer.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+  sketchContainer.style.gridTemplateRows = `repeat(${value}, 1fr)`;
+  document.querySelectorAll('.grid').forEach(e => e.remove());
+  for (i=0; i <= numDiv; i++) {
+     let divs = document.createElement('div');
+     divs.classList.add("grid");
+     divs.addEventListener ('mouseover', () => {
+      divs.style.backgroundColor = "black";
+     })
+    sketchContainer.appendChild(divs);
+  }
+  
+};
+//Also possible with sketchContainer.insertAdjacentElement("beforeend", divs);  very usefull 
+// becouse many possibilities instead of just "beforeend". Instead of using appendChild
+
+
+inputElement.addEventListener('input', () => {
+  value = inputElement.value;
+  createGrid(value);
+  return value;
+});
+
+
+
+
+
+
+
