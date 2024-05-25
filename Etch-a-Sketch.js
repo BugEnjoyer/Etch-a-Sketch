3cfc6ded9a7e8913e165e22547b657b86346d15d
kevinR <kevinroesner94@gmail.com>
2024-05-25 13:27:38 +0200

    Nearly all wished functions working. Random-color need some changes.

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,6 +1,8 @@
 const sketchContainer = document.querySelector('.draw-flex-container');
-let inputElement = document.querySelector('.input-value');
+const inputElement = document.querySelector('.input-value');
+const colorInput = document.querySelector('.color-input');
 let value = inputElement.value;
+let colorValue = "#000000";
 let numDiv = value*value;
 function createGrid(value){
   numDiv = value*value;
@@ -10,12 +12,9 @@ function createGrid(value){
   for (i=0; i <= numDiv; i++) {
      let divs = document.createElement('div');
      divs.classList.add("grid");
-     divs.addEventListener ('mouseover', () => {
-      divs.style.backgroundColor = "black";
-     })
+     divs.addEventListener ('mouseover', applyColor)
     sketchContainer.appendChild(divs);
   }
-  
 };
 //Also possible with sketchContainer.insertAdjacentElement("beforeend", divs);  very usefull 
 // becouse many possibilities instead of just "beforeend". Instead of using appendChild
@@ -26,6 +25,40 @@ inputElement.addEventListener('input', () => {
   createGrid(value);
   return value;
 });
+
+
+
+colorInput.addEventListener('input', () => {
+  colorValue = colorInput.value;
+  return colorValue;
+});
+
+const eraser = document.querySelector('.eraser-btn');
+eraser.addEventListener('click', () => colorValue = "#FFFFFF");
+
+const clearBtn = document.querySelector('.clear-btn');
+clearBtn.addEventListener('click', () => {
+  clearBoard();
+});
+
+function clearBoard() {
+  let divs = document.querySelectorAll('.grid');
+  divs.forEach((divs) => divs.style.backgroundColor = "white");
+};
+
+const randomColor = document.querySelector('.rainbow-btn');
+randomColor.addEventListener('click', () => colorValue = `hsl(${Math.random() * 360 }, 100%, 50%)`);
+
+function applyColor () {
+  this.style.backgroundColor = `${colorValue}`;
+}
+
+function setColor() {
+  colorValue = color;
+}
+
+
+
 
 
 
